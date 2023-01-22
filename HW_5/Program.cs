2fc34fd42a29b2c4fca3bd6a6da85425a6672895
StasySamars <119619342+StasySamars@users.noreply.github.com>
2023-01-22 14:54:33 +0300

    HW 5 & 6 - DONE

---
+++ HW_5/Program.cs
@@ -0,0 +1,141 @@
+﻿// N - Длина массива, min и max значение
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+//Вывод массива int
+void ShowArrayInt(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+/* /* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+//Функция определения четных чисел в массиве.
+int NumEvenInArray(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+
+
+
+/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+ */
+
+//Функция суммирования нечетных чисел в массиве
+int SumOddInArray(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 1)
+        {
+            result += array[i];
+        }
+    }
+    return result;
+}
+
+//Функция суммирования чисел стоящих на нечетных позициях
+int SumOddPositionInArray(int[] array)
+{
+    int result = 0;
+    for (int i = 1; i < array.Length; i+=2) {result += array[i];}
+    return result;
+}
+
+
+
+/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и 
+минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */ 
+
+//Создание и возврат массива с рандомными значениями типа double
+
+double[] CreateRandomArrayDouble(int N, int start, int end)
+{
+
+    double[] RandomArray = new double[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end) + (new Random().NextDouble());
+    }
+    return RandomArray;
+    //double[] CreateRandomArrayDouble(int N, int start, int end, int simbolAfter)
+    //double dot = Math.Pow(10, simbolAfter);
+    //+ (((new Random().Next()) % Math.Pow(10, simbolAfter)) * 0.01)
+}
+//Вывод масива double
+void ShowArrayDouble(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+//Разница между max и min числами массива
+void DiffMaxMinArray(double[] array)
+{
+    double max = 0;
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        else if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    Console.WriteLine($"max = {max:f3}, min = {min:f3} разница = {max - min:f3}");
+}
+
+
+Console.Clear();
+int[] arrayInt = CreateRandomArray(6, 100, 1000);               //Массив с рандомными числами
+Console.Write($"Массив ");
+ShowArrayInt(arrayInt);                                         //Отображение массива
+
+Console.WriteLine($"Колличество " +
+$"четных чисел в массиве = {NumEvenInArray(arrayInt)}");        //Задача №34
+
+Console.WriteLine($"Сумма чисел массива " +
+$"на нечетных позициях = {SumOddPositionInArray(arrayInt)}");   //Задача №36
+
+
+Console.Write($"Вещественный массив ");                         //Задача №38
+
+double[] arrayDouble = CreateRandomArrayDouble(6, 100, 1000);   //Создание массива
+ShowArrayDouble(arrayDouble);                                   //Отображение
+DiffMaxMinArray(arrayDouble);                                   //Разница между min и max в массиве
+
+
---
+++ HW_6/Program.cs
@@ -0,0 +1,74 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+void Number()
+{
+    Console.Clear();
+    Console.WriteLine("Для остановки программы нажите 'q'.\nВведите любые числа и я посчитаю сколько из них больше 0: ");
+    bool GoCode = true;
+    int result = 0;
+    while (GoCode)
+    {
+        string m = Console.ReadLine();
+        if (int.TryParse(m, out int n) && Convert.ToInt32(m) > 0)
+        {
+            result++;
+        }
+        else if (m == "q")
+        {
+            Console.Write($"Вы нажали 'q', Программа остановилась. Введено {result} чисел(а) больше 0.\n");
+            GoCode = false;
+        }
+    }
+}
+
+Number();
+
+
+/* Программа остановится в тот момент когда будет введена “q” до тех пор,
+программа считает сколько чисел больше 0 ввел пользователь  */
+
+
+
+
+
+
+
+/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.WriteLine("Прямая 1. Введите k1");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Прямая 1. Введите b1");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Прямая 2. Введите k2");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Прямая 2. Введите b2");
+double b2 = double.Parse(Console.ReadLine());
+
+void CoordinatePointLine(double k1, double k2, double b1, double b2)
+{
+    Console.Write("Уравнение пересечения прямых:\ny = k1 * x + b1\ny = k2 * x + b1\n\n");
+    if (k1 != k2)
+    {
+        double x = (b1 - b2) / (k2 - k1);
+        double y = k1 * x + b1;
+        // double y2 = k2 * x + b2;                 //y2 == y1
+        Console.Write($"x = {x} y1 = {y}");
+    }
+    else if (k1 == k2 && b1 != b2)
+    {
+        Console.Write("Две прямые параллельны, a значит, они никогда не пересекутся и система не будет иметь решений");
+
+    }
+    else if (k1 == k2 && b1 == b2)
+    {
+        Console.Write("Две прямые совпали, каждая точка будет решением, а у системы будет бесчисленное множество решений.");
+    }
+}
+
+CoordinatePointLine(k1, k2, b1, b2);
