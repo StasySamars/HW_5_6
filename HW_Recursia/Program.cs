d61c9d8b83aca52a961fded593e87abd6668f391
StasySamars <119619342+StasySamars@users.noreply.github.com>
2023-02-09 13:54:26 +0300

    HW_Recursia done

--- HW_Recursia/Program.cs
+++ HW_Recursia/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача №68
+Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+m = 2, n = 3 -> A(m,n) = 9
+
+m = 3, n = 2 -> A(m,n) = 29
+Console.WriteLine("Введите M");
+int mA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите N");
+int nA = int.Parse(Console.ReadLine());
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ackermann(m - 1, 1);
+    else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+
+Console.WriteLine(Ackermann(mA, nA)); */
+
+
+
+/* Задача №66
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120M = 4; N = 8. -> 30
+Console.WriteLine("Введите M");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите N");
+int n = int.Parse(Console.ReadLine());
+
+int SumOfInterval(int firstNum, int secondNum)
+{
+
+    int sum = 0;
+    if (firstNum == secondNum) return secondNum;
+    sum = firstNum + SumOfInterval(firstNum + 1, secondNum);
+    Console.WriteLine($"{firstNum} + {secondNum} = {sum}");
+    return sum;
+}
+
+Console.WriteLine(SumOfInterval(m, n)); */
+
+
+
+
+
+/* Задача №64
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+
+void PrintNumbers(int start)
+{
+    if (start < 1) return;
+    Console.Write($"{start} ");
+    if (start == 1) return;
+    PrintNumbers(start - 1);
+}
+
+PrintNumbers(N); */
